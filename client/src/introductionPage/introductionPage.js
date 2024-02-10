25791416c42f11e05b8bca4e308b7f4c6cd6df52
Smile <singhswapnil060303@gmail.com>
2024-02-10 23:22:37 +0530

    Fix error in introductionPage.js

--- client/src/introductionPage/introductionPage.js
+++ client/src/introductionPage/introductionPage.js
@@ -4,10 +4,10 @@ const introductionPage =(props) => {
     return (
         <div className='introduction_page_container'>
             <div className='introduction_page_panel'>
-              <img src = {logo} className='introduction_page_image'></img>
+                <img src = {logo} className='introduction_page_image'></img>
             </div>
         </div>
     )
 }
 
-export default introductionPage
+export default introductionPage 
