32964b5d6b67e916c3092e1b09d8ced34c38c2f2
Smile <singhswapnil060303@gmail.com>
2024-02-12 23:40:59 +0530

    Add logo and buttons to introduction page

--- client/src/introductionPage/introductionPage.js
+++ client/src/introductionPage/introductionPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import logo from "../resources/images/logo.png"
 
 const introductionPage =(props) => {
     return (
         <div className='introduction_page_container'>
             <div className='introduction_page_panel'>
                 <img src = {logo} className='introduction_page_image'></img>
+                Buttons
             </div>
         </div>
     )
