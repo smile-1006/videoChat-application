57c57553128a0517b15089174deb37a107d670c5
Smile <singhswapnil060303@gmail.com>
2024-02-14 20:05:38 +0530

    Add ConnectingButtons component to IntroductionPage

--- client/src/introductionPage/introductionPage.js
+++ client/src/introductionPage/introductionPage.js
@@ -2,13 +2,14 @@ import React from 'react'
 import logo from "../resources/images/logo.png"
 
 import "./IntroductionPage.css"
+import ConnectingButtons from './ConnectingButtons'
 
 const introductionPage =(props) => {
     return (
         <div className='introduction_page_container'>
             <div className='introduction_page_panel'>
                 <img src = {logo} className='introduction_page_image'></img>
-                Buttons
+                <ConnectingButtons/>
             </div>
         </div>
     )
