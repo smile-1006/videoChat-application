57b5a2cd4c08c7b43157a5e8c08cce38c4b28aa2
Smile <singhswapnil060303@gmail.com>
2024-02-16 23:49:01 +0530

    Add setIsRoomHostAction to joinRoomPage.js

--- client/src/joinRoomPage/joinRoomPage.js
+++ client/src/joinRoomPage/joinRoomPage.js
@@ -1,16 +1,19 @@
 import React, { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { setIsRoomHost } from '../store/actions'
 
 import './joinRoomPage.css'
 
 const joinRoomPage = (props) => {
-
+  const { setIsRoomHostAction } = props;
   const search = useLocation().search;
 
   useEffect(() => {
     const isRoomHost = new URLSearchParams(search).get('host');
     if (isRoomHost) {
       //setting in our redux store that user is host
+      setIsRoomHostAction(true);
     }
   }, [])
 
@@ -21,6 +24,19 @@ const joinRoomPage = (props) => {
         </div>
     </div>
   )
+}
+
+const mapStoreStateToProps = (state) => {
+  return {
+    ...state
+  }
+}
+
+const mapActionsToProps = (dispatch) => {
+  return {
+    //setting in our redux store that user is host
+    setIsRoomHostAction:(isRoomHost) => dispatch(setIsRoomHost(isRoomHost))
+  }
 }
 
 export default joinRoomPage
