b663805b83d380285ce62abddb21b18fb086631f
Smile <singhswapnil060303@gmail.com>
2024-02-20 21:49:19 +0530

    Refactor JoinRoomInputs component

--- client/src/joinRoomPage/JoinRoomInputs.js
+++ client/src/joinRoomPage/JoinRoomInputs.js
@@ -1,11 +1,34 @@
 import React from 'react'
 
-const JoinRoomInputs = () => {
+const Input = ({placeholder, value, changeHandler}) => {
+    return(
+        <input
+        value={value}
+        onChange={changeHandler}
+        className='join_room_input'
+        placeholder={placeholder}
+        
+        />
+    )
+}
+const JoinRoomInputs = (props) => {
+    const {roomIdValue, setRoomIdValue, nameValue, setNameValue, isRoomHost} = props;
+    
+    const handleRoomIdValueChange = (event) => {
+        setRoomIdValue(event.target.value);
+    }
+
+    const handleNameIdValueChange = (event) => {
+        setNameValue(event.target.value);
+    }
+
     return (
-    <div>
+    <div className='join_room_inputs_container'>
         Inputs
     </div>
     )
 }
 
 export default JoinRoomInputs
+
+
