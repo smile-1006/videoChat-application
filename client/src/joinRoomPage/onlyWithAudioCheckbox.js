b7c7109f571376901ed44c5e6dc166bbd1ebe81d
Smile <singhswapnil060303@gmail.com>
2024-03-05 23:50:32 +0530

    Add onlyWithAudioCheckbox component and check.png image

---
+++ client/src/joinRoomPage/onlyWithAudioCheckbox.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import CheckImg from '../resources/images/check.png'
+
+const onlyWithAudioCheckbox = () => {
+    const handleConnectionTypeChange = () => {
+        console.log('connection type changed');
+    }
+
+    return (
+    <div className='checkbox_container' >
+        <div className='checkbox_connection' onClick={handleConnectionTypeChange}>  
+            <img className='checkbox_image' src={CheckImg}></img>
+        </div>
+        <p className='checkbox_container_paragraph'>only audio</p>
+    </div>
+    )
+}
+
+export default onlyWithAudioCheckbox
