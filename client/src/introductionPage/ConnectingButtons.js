ae880c09341b3f20f311b1a01ccbf1fef75b523c
Smile <singhswapnil060303@gmail.com>
2024-04-25 17:49:34 +0530

    Refactor ConnectingButton and ConnectingButtons components

--- client/src/introductionPage/ConnectingButtons.js
+++ client/src/introductionPage/ConnectingButtons.js
@@ -1,22 +1,31 @@
-import React from 'react'
-import ConnectingButton from './ConnectingButton'
-import {useHistory} from "react-router-dom"
+import React from "react";
+import ConnectingButton from "./ConnectingButton";
+import { useHistory } from "react-router-dom";
 
 const ConnectingButtons = () => {
-    const history = useHistory();
-    const pushToJoinRoomPage = () => {
-        history.push('/join-room')
-    }
-    const pushToRoomPageAsHost = () => {
-        history.push('/join-room?host=true')
-    }
-    return (
-    <div className='connecting_buttons_container'>
-        <ConnectingButton buttonText='join a meeting' onClickHandler={pushToJoinRoomPage}/>
-        <ConnectingButton createRoomButton buttonText='host a meeting' onClickHandler={pushToRoomPageAsHost}/>
-        
-    </div>
-    )
-}
+  let history = useHistory();
 
-export default ConnectingButtons
+  const pushToJoinRoomPage = () => {
+    history.push("/join-room");
+  };
+
+  const pushToJoinRoomPageAsHost = () => {
+    history.push("/join-room?host=true");
+  };
+
+  return (
+    <div className="connecting_buttons_container">
+      <ConnectingButton
+        buttonText="Join a meeting"
+        onClickHandler={pushToJoinRoomPage}
+      />
+      <ConnectingButton
+        createRoomButton
+        buttonText="Host a meeting"
+        onClickHandler={pushToJoinRoomPageAsHost}
+      />
+    </div>
+  );
+};
+
+export default ConnectingButtons;
