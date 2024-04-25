bff877029cc68b6145210e1d90e5006096393113
Smile <singhswapnil060303@gmail.com>
2024-04-25 16:45:14 +0530

    Add socket.io connection and service worker

--- client/src/App.js
+++ client/src/App.js
@@ -6,19 +6,26 @@ import IntroductionPage from './introductionPage/introductionPage';
 import JoinRoomPage from './JoinRoomPage/JoinRoomPage';
 import RoomPage from './Roompage/RoomPage';
 
+import { connectWithSocketIOServer } from "./utils/wss";
+
+
 function App() {
+  useEffect(() => {
+    connectWithSocketIOServer();
+  }, []);
+
   return (
     <Router>
         <Switch>
-          <Route path= '/join-room'>
+          <Route path= "/join-room">
             <JoinRoomPage/>
 
           </Route>
-          <Route path= '/room'>
+          <Route path= "/room">
             <RoomPage/>
 
           </Route>
-          <Route path= '/'>
+          <Route path= "/">
             <IntroductionPage/>
 
           </Route>
