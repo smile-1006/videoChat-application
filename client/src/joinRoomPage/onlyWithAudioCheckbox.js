d0ace14c65967eda9f11100dc2a08ee6ccd97ca2
Smile <singhswapnil060303@gmail.com>
2024-04-25 17:51:54 +0530

    Refactor OnlyWithAudioCheckbox component

--- client/src/joinRoomPage/onlyWithAudioCheckbox.js
+++ client/src/joinRoomPage/onlyWithAudioCheckbox.js
@@ -1,19 +1,24 @@
-import React from 'react'
-import CheckImg from '../resources/images/check.png'
+import React from "react";
+import CheckImg from "../resources/images/check.png";
 
-const OnlyWithAudioCheckbox = () => {
-    const handleConnectionTypeChange = () => {
-        console.log('connection type changed');
-    }
+const OnlyWithAudioCheckbox = ({
+  connectOnlyWithAudio,
+  setConnectOnlyWithAudio,
+}) => {
+  const handleConnectionTypeChange = () => {
+    setConnectOnlyWithAudio(!connectOnlyWithAudio);
+  };
 
-    return (
-    <div className='checkbox_container' >
-        <div className='checkbox_connection' onClick={handleConnectionTypeChange}>  
-            <img className='checkbox_image' src={CheckImg}></img>
-        </div>
-        <p className='checkbox_container_paragraph'>only audio</p>
+  return (
+    <div className="checkbox_container">
+      <div className="checkbox_connection" onClick={handleConnectionTypeChange}>
+        {connectOnlyWithAudio && (
+          <img className="checkbox_image" src={CheckImg}></img>
+        )}
+      </div>
+      <p className="checkbox_container_paragraph">Only audio</p>
     </div>
-    )
-}
+  );
+};
 
-export default OnlyWithAudioCheckbox
+export default OnlyWithAudioCheckbox;
